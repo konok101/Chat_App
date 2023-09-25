43b0396313bd30b9e12aeb6cc04d658cb35f1508
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-09-26 02:27:28 +0600

    chat app initialize

---
+++ App.js
@@ -0,0 +1,51 @@
+//External imports
+const express = require("express");
+const dotenv = require("dotenv");
+const mongose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+//Internal Imports
+const {notFoundHandler, errorHandler}=require('./middlewares/common/errorHandler')
+
+
+const app = express();
+dotenv.config();
+
+mongose.connect(process.env.CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+})
+    .then(() => console.log('dataBase Connect succesfully !!!'))
+    .catch((err) => console.log(err))
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', "ejs");
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//parse  cookie 
+app.use(cookieParser(process.env.COOKIE_SECRT));
+
+
+
+
+
+//error or 404 handler
+app.use(notFoundHandler)
+
+//common error handler
+app.use(errorHandler)
+
+
+
+
+
+app.listen(5000, () => {
+    console.log('run this app');
+})
